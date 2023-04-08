9829f29b316e57fcc85c3c48ae5b45f287626499
azertyIA <47957207+azertyIA@users.noreply.github.com>
2023-04-07 17:30:17 -0700

    Image Quality Problem Fixed and Local FIle Support
    I just zoom in to the max settings, scrape  the canvas, and send a message back to popup.js to rezoom the canvas. I also "added" local file support, so the zipped page doesn't need to be edited in string literals, but rather actual files.

--- download.js
+++ download.js
@@ -1,142 +1,79 @@
-(function () {
+(() => {
+  // not sure what use strict does
   "use strict";
 
-  const startImages = () => {
-    const zip = new JSZip();
-    const images = zip.folder("images");
-    const canvases = document.getElementsByClassName("sketch-surface");
+  // initialize JSZip object (called in popup.js)
+  const zip = new JSZip();
+  // create a folder called images in that zip
+  const images = zip.folder("images");
 
-    if (canvases.length == 0) return;
-    for (let i = 0; i < canvases.length; i++) {
-      images.file(`image-${i}.png`, ctoi(canvases[i]));
-    }
+  // find all elements of class "sketch-surface" (all of them are canvases)
+  // and should be implied to have the canvas tag
+  const canvases = document.getElementsByClassName("sketch-surface");
 
-    zip.file(
-      "index.html",
-      `
-      <!DOCTYPE html>
-      <html>
-      <head>
-        <title>Two Images on Top of Each Other BRUH</title>
-        <link rel="stylesheet" href="index.css" />
-      </head>
-      <body>
-        <img id="image1">
-        <img id="image2">
-        <div style="margin-bottom:665px"></div>
-        <button style="margin-left:50px; padding:50px;"onclick="decrementCounter()">Previous</button>
-        <button style="padding:50px;"onclick="onOff()">On/Off</button>
-        <button style="padding:50px;"onclick="incrementCounter()">Next</button>
-        <script src="script.js"></script>
-      </body>
-      </html>      
-      `
+  // if there are no canvases, don't proceed
+  if (canvases.length == 0) return;
+  // for each canvas, create a file in the image folder using "ctoi"
+  for (let i = 0; i < canvases.length; i++) {
+    images.file(`image-${i}.png`, ctoi(canvases[i]));
+  }
+  // after finished scraping canvases, resize window
+  chrome.runtime.sendMessage({ message: "resize" });
+
+  // find the "original background" element and extract the URL source
+  const div = document.getElementsByClassName("image-background-div")[0];
+  const computedStyle = window.getComputedStyle(div);
+  const backgroundImage = computedStyle.backgroundImage;
+
+  const imageUrl = backgroundImage
+    .replace(/^url\(["']?/, "")
+    .replace(/["']?\)$/, "");
+
+  // list of files to download and put in zip
+  // :: new change: can import from other files using runtime.GetURL
+  const files = [
+    [imageUrl, "og.png"],
+    [chrome.runtime.getURL("previewer/index.html"), "index.html"],
+    [chrome.runtime.getURL("previewer/script.js"), "script.js"],
+    [chrome.runtime.getURL("previewer/index.css"), "index.css"],
+  ];
+
+  // create a list of requests and push all the URL fetches to that
+  let requests = [];
+
+  files.forEach(([url, fileName]) => {
+    // fetch the URL image and store its blob
+    requests.push(
+      fetch(url)
+        .then((response) => response.blob())
+        .then((blob) => zip.file(fileName, blob))
     );
+  });
 
-    zip.file(
-      "index.css",
-      `
-      html {
-        height: 100%;
-        width: 100%;
-      }
-      
-      #image1,
-      #image2 {
-        position: absolute;
-        top: 15px;
-        left: 25px;
-        width: 650px;
-        height: 650px;
-      }
-      
-      #image2 {
-        z-index: -1;
-      }
-
-      #image2.hidden {
-        display: none;
-      }
-      `
-    );
-
-    zip.file(
-      "script.js",
-      `
-      let counter = 2; // initialize the counter variable
-      let isOn = false;
-      document.getElementById("image2").src = "og.png";
-  
-      function onOff() {
-        if (isOn == false) {
-          isOn = true;
-          document.getElementById("image2").className = "";
-        } else {
-          isOn = false;
-          document.getElementById("image2").className = "hidden";
-        }
-      }
-      function incrementCounter() {
-        counter++; // increment the counter variable
-        var imagePath1 = "images/image-" + counter + ".png";
-        document.getElementById("image1").src = imagePath1;
-      }
-  
-      function decrementCounter() {
-        if (counter > 0) counter--; // decrement the counter variable
-        var imagePath1 = "images/image-" + counter + ".png";
-        document.getElementById("image1").src = imagePath1;
-      }
-  
-      decrementCounter();
-      onOff();
-      `
-    );
-
-    var div = document.getElementsByClassName("image-background-div")[0];
-    var computedStyle = window.getComputedStyle(div);
-    var backgroundImage = computedStyle.backgroundImage;
-
-    var imageUrl = backgroundImage
-      .replace(/^url\(["']?/, "")
-      .replace(/["']?\)$/, "");
-
-    const files = [[imageUrl, "og.png"]];
-    let requests = [];
-
-    files.forEach(([url, fileName]) => {
-      requests.push(
-        fetch(url)
-          .then((response) => response.blob())
-          .then((blob) => zip.file(fileName, blob))
-      );
+  // after all the requests are done...
+  Promise.all(requests)
+    .then(() => zip.generateAsync({ type: "blob" }))
+    .then((content) => {
+      // create a download link and click it
+      var downloadLink = document.createElement("a");
+      downloadLink.href = URL.createObjectURL(content);
+      downloadLink.download = "canvases.zip";
+      downloadLink.click();
     });
-
-    Promise.all(requests).then(() => {
-      zip.generateAsync({ type: "blob" }).then((content) => {
-        console.log(3);
-        var downloadLink = document.createElement("a");
-        downloadLink.href = URL.createObjectURL(content);
-        downloadLink.download = "canvases.zip";
-        downloadLink.click();
-      });
-    });
-
-    console.log(2);
-  };
 
   // canvas to image
-  const ctoi = (canvas) => {
+  function ctoi(canvas) {
+    // converts canvas element to DataURL
     const dataURL = canvas.toDataURL("image/png");
+    // separate character bytes and get the number associated
     const byteCharacters = atob(dataURL.split(",")[1]);
-    const byteNumbers = new Array(byteCharacters.length);
+    const byteNumber = new Array(byteCharacters.length);
     for (let i = 0; i < byteCharacters.length; i++) {
-      byteNumbers[i] = byteCharacters.charCodeAt(i);
+      byteNumber[i] = byteCharacters.charCodeAt(i);
     }
-
-    const byteArray = new Uint8Array(byteNumbers);
+    // turn that into a byte array and return it as a png blob
+    const byteArray = new Uint8Array(byteNumber);
     const blob = new Blob([byteArray], { type: "image/png" });
     return blob;
-  };
-  startImages();
+  }
 })();
--- popup.js
+++ popup.js
@@ -1,14 +1,35 @@
-(function () {
+(() => {
+  // not sure what use strict does
   "use strict";
+
+  // basic getbyID stuff and if clicked
+  let prevZoom = 1;
+
   document
     .getElementById("desmosDashboardDownload")
-    .addEventListener("click", function () {
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.scripting.executeScript({
-          target: { tabId: tabs[0].id, allFrames: false },
-          files: ["/jszip.min.js", "/download.js"],
+    .addEventListener("click", () => {
+      // get the zoom and then
+      chrome.tabs.getZoom().then((zoom) => {
+        prevZoom = zoom;
+        // increase the zoom to enhance quality
+        chrome.tabs.setZoom(5);
+        // fetch the active tabs
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+          // execute the JSZip library and download.js
+          chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id, allFrames: false },
+            files: ["/jszip.min.js", "/download.js"],
+          });
         });
       });
+
+      // get the resize window message to bring it back to original
+      chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+        if (request.message !== "resize") return;
+        chrome.tabs.setZoom(prevZoom);
+      });
+
+      // and close the window immediately after clicking
       window.close();
     });
 })();
