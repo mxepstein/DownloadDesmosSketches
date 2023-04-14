5dc8377081fd61bac8918bfaa2ad648339c60c8c
mxepstein <mxepstein@mxepstein.com>
2023-04-14 07:11:22 -0700

    Version 2
    Has asynchronous bugs

---
+++ popup/popup.js
@@ -0,0 +1,36 @@
+  const sendMessageButton = document.getElementById("greenButton");
+  sendMessageButton.onclick = async function(e) {
+      let queryOptions = { active: true, currentWindow: true };
+      let tab = await chrome.tabs.query(queryOptions);
+      var prevZoom;
+
+      // Zoom in
+      chrome.tabs.getZoom().then((zoom) => {
+          prevZoom = zoom; // Get the current zoom status
+          chrome.tabs.setZoom(5).then(() => {
+
+
+            //////////////////////////////////
+               chrome.tabs.sendMessage(
+                  tab[0].id, {
+                      message: "doStuff",
+                  },
+                  function(response) {
+                      //chrome.tabs.setZoom(1);
+                  }
+              );
+              ////////////////////////////////
+
+
+          });
+      });
+
+
+
+
+     
+      //Could the message just be sent here instead?
+
+      //window.close();
+
+  };
