894d3d36a1d9a221da4a501adc39251a482ce3f4
mxepstein <mxepstein@mxepstein.com>
2023-04-14 23:26:03 -0700

    Clean up and new template

---
+++ galleryTemplate/script.js
@@ -0,0 +1,48 @@
+
+/*
+const backgroundGridContainer = document.querySelector('.background-container .grid-container');
+const foregroundGridContainer = document.querySelector('.foreground-container .grid-container');
+for (let i = 1; i <= 100; i++) {
+  const backgroundImg = document.createElement('img');
+  backgroundImg.src = `background.png`;
+  backgroundGridContainer.appendChild(backgroundImg);
+  
+  const foregroundImg = document.createElement('img');
+  foregroundImg.src = `images/image-${i}.png`;
+  foregroundGridContainer.appendChild(foregroundImg);
+}
+*/
+
+
+
+const backgroundGridContainer = document.querySelector('.background-container .grid-container');
+const foregroundGridContainer = document.querySelector('.foreground-container .grid-container');
+for (let i = 1; i <= 1000; i++) {
+  const backgroundImg = document.createElement('img');
+  backgroundImg.src = `background.png`;
+  backgroundGridContainer.appendChild(backgroundImg);
+  const foregroundImg = document.createElement('img');
+  foregroundImg.src = `images/image-${i}.png`;
+  foregroundGridContainer.appendChild(foregroundImg);
+
+    }
+
+
+
+const slider = document.getElementById('slider');
+slider.addEventListener('input', function() {
+  const value = this.value;
+  document.documentElement.style.setProperty('--slider-value', value + 'px');
+});
+
+
+
+
+const toggleButton = document.getElementById('toggle-background');
+toggleButton.addEventListener('click', function() {
+  if (backgroundGridContainer.style.display === 'none') {
+    backgroundGridContainer.style.display = 'grid';
+  } else {
+    backgroundGridContainer.style.display = 'none';
+  }
+});
--- popup/popup.js
+++ popup/popup.js
@@ -1,31 +1,25 @@
 const sendMessageButton = document.getElementById("greenButton");
+
 sendMessageButton.onclick = async function(e) {
+
     let queryOptions = { active: true, currentWindow: true };
     let tab = await chrome.tabs.query(queryOptions);
     var prevZoom;
 
-
-    // Zoom in
     chrome.tabs.getZoom().then((zoom) => {
         prevZoom = zoom; // Get the current zoom status
         chrome.tabs.setZoom(5).then(() => {
-
-
             //////////////////////////////////
             chrome.tabs.sendMessage(
                 tab[0].id, {
-                    message: "Please download the zip",
+                    message: "Please get the sketches and download the zip",
                 },
                 function(response) {
                     chrome.tabs.setZoom(prevZoom);
                 }
             );
             ////////////////////////////////
-
-
         });
     });
-
     //window.close();
-
 };
