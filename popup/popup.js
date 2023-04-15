b1a02a69a4383113537ff3dde7def345051d4480
mxepstein <mxepstein@mxepstein.com>
2023-04-14 20:28:19 -0700

    clean up

--- popup/popup.js
+++ popup/popup.js
@@ -1,37 +1,31 @@
-  const sendMessageButton = document.getElementById("greenButton");
-  sendMessageButton.onclick = async function(e) {
-      let queryOptions = { active: true, currentWindow: true };
-      let tab = await chrome.tabs.query(queryOptions);
-      var prevZoom;
+const sendMessageButton = document.getElementById("greenButton");
+sendMessageButton.onclick = async function(e) {
+    let queryOptions = { active: true, currentWindow: true };
+    let tab = await chrome.tabs.query(queryOptions);
+    var prevZoom;
 
 
-      // Zoom in
-      chrome.tabs.getZoom().then((zoom) => {
-          prevZoom = zoom; // Get the current zoom status
-          chrome.tabs.setZoom(5).then(() => {
+    // Zoom in
+    chrome.tabs.getZoom().then((zoom) => {
+        prevZoom = zoom; // Get the current zoom status
+        chrome.tabs.setZoom(5).then(() => {
 
 
             //////////////////////////////////
-               chrome.tabs.sendMessage(
-                  tab[0].id, {
-                      message: "doStuff",
-                  },
-                  function(response) {
-                      chrome.tabs.setZoom(prevZoom);
-                  }
-              );
-              ////////////////////////////////
+            chrome.tabs.sendMessage(
+                tab[0].id, {
+                    message: "Please download the zip",
+                },
+                function(response) {
+                    chrome.tabs.setZoom(prevZoom);
+                }
+            );
+            ////////////////////////////////
 
 
-          });
-      });
+        });
+    });
 
+    //window.close();
 
-
-
-     
-      //Could the message just be sent here instead?
-
-      //window.close();
-
-  };
+};
